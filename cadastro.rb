b48887edfc9aa678decc2f78353fa9e82d2cb25d
Esdrasborges <105466934+Esdrasborges@users.noreply.github.com>
2023-10-03 20:46:45 -0300

    Criando o primeiro metodo

--- cadastro.rb
+++ cadastro.rb
@@ -9,5 +9,21 @@ class Cliente
         @numero_cadastro = numero_cadastro
     end
 
-    
+    def avaliar_cpf
+        puts "Qual o numero do seu CPF?: "
+        self.cpf = gets.chomp
+        if CPF.valid?(@cpf)
+            puts "O número do CPF é válido! Por favor, continue seu cadastro"
+            puts "Qual o seu nome? "
+            @nome = gets.chomp 
+            puts "Qual o seu Enderço? "
+            @endereco = gets.chomp
+            puts "Qual o seu Número de Telefone? "
+            @numero_tele = gets.chomp.to_s
+            puts "Seu Cadastro Foi conluido com Sucesso, observe com atenção sua ficha"
+            puts "Nome = #{@nome}, CPF = #{@cpf}, Endereço = #{@endereco}, Numero de Telefone =  #{@numero_tele.to_s}, Numero de Cadastro = #{@numero_cadastro.to_s}"
+        else
+            puts "O número do CPF não é valido! Por favor digite um novo."
+        end
+    end
 end
