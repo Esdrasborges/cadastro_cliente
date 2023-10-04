282d6085c416a3f93d7d3069bf6da97060335086
Esdrasborges <105466934+Esdrasborges@users.noreply.github.com>
2023-10-03 21:47:27 -0300

    usando meio de repetição para não sair do codigo

--- cadastro.rb
+++ cadastro.rb
@@ -1,7 +1,10 @@
+require "cpf_cnpj"
+
 class Cliente   
     attr_accessor :nome, :endereco, :numero_tele, :cpf
     attr_writer :numero_cadastro
-    def initialize (nome , cpf,  endereco, numero_tele, numero_cadastro)
+
+    def initialize(nome, cpf, endereco, numero_tele, numero_cadastro)
         @nome = nome
         @cpf = cpf
         @endereco = endereco
@@ -10,20 +13,29 @@ class Cliente
     end
 
     def avaliar_cpf
-        puts "Qual o numero do seu CPF?: "
-        self.cpf = gets.chomp
-        if CPF.valid?(@cpf)
-            puts "O número do CPF é válido! Por favor, continue seu cadastro"
-            puts "Qual o seu nome? "
-            @nome = gets.chomp 
-            puts "Qual o seu Enderço? "
-            @endereco = gets.chomp
-            puts "Qual o seu Número de Telefone? "
-            @numero_tele = gets.chomp.to_s
-            puts "Seu Cadastro Foi conluido com Sucesso, observe com atenção sua ficha"
-            puts "Nome = #{@nome}, CPF = #{@cpf}, Endereço = #{@endereco}, Numero de Telefone =  #{@numero_tele.to_s}, Numero de Cadastro = #{@numero_cadastro.to_s}"
-        else
-            puts "O número do CPF não é valido! Por favor digite um novo."
+        while !CPF.valid?(@cpf)
+            puts "Qual o numero do seu CPF?: "
+            @cpf = gets.chomp
+            if CPF.valid?(@cpf)
+                puts "O número do CPF é válido! Por favor, continue seu cadastro"
+            else
+                puts "O número do CPF não é válido! Por favor, digite um novo."
+            end
         end
+
+        puts "Qual o seu nome? "
+        @nome = gets.chomp 
+        puts "Qual o seu Enderço? "
+        @endereco = gets.chomp
+        puts "Qual o seu Número de Telefone? "
+        @numero_tele = gets.chomp.to_s
+
+        puts "Seu Cadastro Foi concluído com Sucesso, observe com atenção sua ficha"
+        print "Nome = #{@nome}, CPF = #{@cpf}, Endereço = #{@endereco}, Numero de Telefone = #{@numero_tele}, Numero de Cadastro = #{@numero_cadastro}"
     end
 end
+
+cliente1 = Cliente.new("", "", "", "", 1)
+puts "Cadastro do Cliente"
+cliente1.avaliar_cpf
+
